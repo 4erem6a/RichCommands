b6c74a05ade18719d9c33802e406fe488b317309
4erem6a <Sviridoff_E@mail.ru>
2020-01-13 19:48:56 +0300

    Add parseCommand

--- src/parse.ts
+++ src/parse.ts
@@ -1,11 +1,12 @@
 import { RichParserOptions } from "./types/RichParserOptions";
 import { FlagObjectOptions, createFlagObject } from "./utils/createFlagObject";
 import { defaultRichParserOptions } from "./constants";
-import { RichArgv } from "./types/types";
+import { RichArgv, RichCommand } from "./types/types";
 import { RichParser } from "./RichParser";
 import { isArgument } from "./types/isArgument";
 import { isFlag } from "./types/isFlag";
 import { createRichArgv } from "./utils/createRichArgv";
+import { createRichCommand } from "./utils/createRichCommand";
 
 type ParseOptions = RichParserOptions & Partial<FlagObjectOptions>;
 
@@ -28,4 +29,32 @@ export function parseArgs(
   const parts = parser.commandParts();
 
   return createRichArgv(parts, options);
+}
+
+/**
+ * Tries to parse the source string to a {@link RichCommand}.
+ * @param source The source string.
+ * @param options Parsing and processing options.
+ * Parsing options override the {@link defaultRichParserOptions default ones},
+ * if you want to disable a parsing option then you should explicitly set it to undefined.
+ * @returns A rich command if command was parsed successfully, null otherwise.
+ */
+export function parseCommand(
+  source: string,
+  options: ParseOptions = {}
+): RichCommand | null {
+  const parser = new RichParser(source, {
+    ...defaultRichParserOptions,
+    ...options
+  });
+
+  const command = parser.command();
+
+  if (!command) {
+    return null;
+  }
+
+  const richCommand = createRichCommand(command, options);
+
+  return richCommand;
 }
